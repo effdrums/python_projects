53c16494992a1a41a5b5572f3d295bcef88da34a
Efrem Blazquez <efrem.blazquez@csuc.cat>
2018-10-05 13:27:37 +0200

    first commit

---
+++ Artilery3/artillery3.py
@@ -0,0 +1,5 @@
+import menu 
+
+if __name__ == "__main__":
+    MA3 = menu.MenuArtillery3()
+    MA3.run()
---
+++ Artilery3/menu.py
@@ -0,0 +1,7 @@
+class MenuArtillery3: 
+    
+    initial_message = "WELCOME TO ARTILLERY3:"
+
+
+    def run(self):
+        print("WELCOME TO ARTILLERY3")
