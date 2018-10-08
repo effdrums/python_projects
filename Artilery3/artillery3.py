3b30f8de3e2c86d01d02c2e21a0f979223b1d13f
Efrem Blázquez <efrem.blazquez@gmail.com>
2018-10-08 23:23:51 +0200

    drawing pair of tanks using sprite Group

--- Artilery3/artillery3.py
+++ Artilery3/artillery3.py
@@ -1,5 +1,6 @@
 import pygame
 import menu 
+import tank
 
 
 # Define some colors
@@ -12,7 +13,7 @@ RED = (255, 0, 0)
 
         
 
-
+ 
 
 
 if __name__ == "__main__":
@@ -21,6 +22,21 @@ if __name__ == "__main__":
     screen.fill(BLACK)
     canPlay = True
     clock = pygame.time.Clock()
+
+    tank_size = {'x':10, 'y':4}
+
+    tank1 = tank.Tank(RED,tank_size['x'],tank_size['y'])
+    tank2 = tank.Tank(GREEN,tank_size['x'],tank_size['y'])
+
+    tank1.rect.x = 10
+    tank1.rect.y = 140
+
+    tank2.rect.x = 480
+    tank2.rect.y = 140
+
+    all_sprites_list = pygame.sprite.Group()
+    all_sprites_list.add(tank1)
+    all_sprites_list.add(tank2)
 
     while canPlay:
         #TODO CATCH CONTROLS
@@ -33,6 +49,8 @@ if __name__ == "__main__":
         #TODO UPDATE
 
         #DRAW
+        all_sprites_list.draw(screen)
+
         pygame.display.flip()
 
         clock.tick(60)
--- Artilery3/tank.py
+++ Artilery3/tank.py
@@ -2,8 +2,10 @@ import pygame
 
 class Tank(pygame.sprite.Sprite): 
 
-    def _init_(self, _color, _x_pos, _y_pos):
+    def __init__(self, _color, _x_pos, _y_pos):
         print("Creating new Tank...")
+
+        super().__init__()
 
         self.image = pygame.Surface([_x_pos,_y_pos])
         self.image.fill(_color)
